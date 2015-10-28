0fad5cd15d156ad8d9ad6676ac64aa73845dc6e0
Robert Gasparyan <robert.gasparyan@gmail.com>
2015-10-28 14:11:38 +0400

    Minor Modification changes

--- app.rb
+++ app.rb
@@ -17,23 +17,24 @@ get '/visit' do
 end
 
 post '/visit' do
-  user_name = params[:username];
-  user_phone = params[:userphone];
-  user_date = params[:userdate];
-  barbername = params[:barbername]
+  @user_name = params[:username];
+  @user_phone = params[:userphone];
+  @user_date = params[:userdate];
+  @barbername = params[:barbername]
 
-  if user_name.empty?
+
+  if @user_name.empty?
     @error = "Username cannot be empty"
     erb :visit
-    elsif user_phone.empty?
+    elsif @user_phone.empty?
     @error = "Phone number cannot be empty"
     erb :visit
-    elsif user_date.empty?
+    elsif @user_date.empty?
     @error = "Date cannot be empty"
     erb :visit
     else
 
-    combined_data = "#{user_name} - #{user_phone} - #{user_date} - #{barbername}\n\n"
+    combined_data = "#{@user_name} - #{@user_phone} - #{@user_date} - #{@barbername}\n\n"
 
     users_file = File.open "public/visits.txt", "a" do |file|
         file.write(combined_data)
