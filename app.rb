11690b8ef849a59f3b259d7d12ad8dc54ecd6802
Robert Gasparyan <robert.gasparyan@gmail.com>
2015-10-28 13:06:29 +0400

    First Validation Set

--- app.rb
+++ app.rb
@@ -8,6 +8,7 @@ get '/' do
 end
 
 get '/about' do
+  @error = "Something wrong"
   erb :about
 end
 
@@ -21,14 +22,27 @@ post '/visit' do
   user_date = params[:userdate];
   barbername = params[:barbername]
 
-  combined_data = "#{user_name} - #{user_phone} - #{user_date} - #{barbername}\n\n"
+  if user_name.empty?
+    @error = "Username cannot be empty"
+    erb :visit
+    elsif user_phone.empty?
+    @error = "Phone number cannot be empty"
+    erb :visit
+    elsif user_date.empty?
+    @error = "Date cannot be empty"
+    erb :visit
+    else
 
-  users_file = File.open "public/visits.txt", "a" do |file|
-  file.write(combined_data)
-  file.write "\n"
+    combined_data = "#{user_name} - #{user_phone} - #{user_date} - #{barbername}\n\n"
+
+    users_file = File.open "public/visits.txt", "a" do |file|
+        file.write(combined_data)
+        file.write "\n"
+      end
+      @message = "Complete"
+      erb :thankyou
   end
-  @message = "Complete"
-  erb :thankyou
+
 end
 
 get '/contacts' do
